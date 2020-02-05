980306a465cc7fde6706be6e4f3d98fa4f25f410
Anna Krusova <anicka.krusova@gmail.com>
2020-02-05 15:42:57 +0100

    comments added

--- password.py
+++ password.py
@@ -1,8 +1,10 @@
+# import modules
 import itertools
 import string
 
 
 def get_char():
+    """creates list of characters from which the combinations of password are going to be made of"""
     lowers = list(string.ascii_lowercase)
     uppers = list(string.ascii_uppercase)
     numbers = [str(i) for i in range(1, 10)]
@@ -11,6 +13,7 @@ def get_char():
 
 
 def input_length():
+    """checks the right input of the length of the password"""
     print('How long do you want your password combinations to be?')
     while True:
         inp = input('Enter an integer between 2 and 6: ')
@@ -27,6 +30,7 @@ def input_length():
 
 
 def pw_gen(characters, length):
+    """generates all characters combinations with selected length and exports them to a text file"""
     with open("password_combinations.txt", "a+") as f:
         #comb_list = []
         for i in itertools.product(characters, repeat=length):
