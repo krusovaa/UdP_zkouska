bf04a822e73f819e6f58480d8a229c2c0d9405d5
Anna Krusova <anicka.krusova@gmail.com>
2020-02-17 21:18:58 +0100

    final version

--- morse_translator.py
+++ morse_translator.py
@@ -1,7 +1,9 @@
 def load_text():
+    """load text to be translated to morse code"""
     while True:
         inp = input('Enter an absolute path to a text file that you want to translate to morse code: ')
         try:
+            # load text file into local variable
             with open(inp, 'r', encoding='utf-8-sig') as f:
                 text = f.read()
                 break
@@ -15,22 +17,27 @@ def load_text():
 
 
 def encrypt(text):
+    """encrypt the text file to morse code and export it to a new text file"""
     cipher = ''
+    # change all letters into uppercase letters, which are used in the morse_code_dict_cz
     text_upper = text.upper()
     for letter in text_upper:
         if letter != ' ':
             if letter in morse_code_dict_cz:
+                # write morse code for selected letter and a space
                 cipher += morse_code_dict_cz[letter] + ' '
             elif letter == '\n':
                 cipher += '\n'
             else:
-                #  character not in morse_code_dict_cz
+                #  if the character is not in morse_code_dict_cz, nothing is added to a cipher
                 cipher += ''
         else:
             cipher += '/ '
     try:
+        # export encrypted text to a new text file
         with open('text_to_morse.txt', 'w') as f:
             f.write(cipher)
+        # open morse code file and translate each line back into text file
         with open('text_to_morse.txt', 'r') as F:
             text_to_morse_lines = F.readlines()
             for line in text_to_morse_lines:
@@ -40,21 +47,25 @@ def encrypt(text):
 
 
 def decrypt(morse_cipher):
+    """decrypt text given as parameter and export it to a new text file"""
     decipher = ''
+    # morse code is split at each space
     morse_cipher_separated = morse_cipher.split(' ')
     for char in morse_cipher_separated:
         if char != ' ':
             if char in inv_morse_code_dict:
+                # write a letter for selected morse code characters
                 decipher += inv_morse_code_dict[char]
             elif char == '/':
                 decipher += ' '
     try:
+        # export decrypted text to a new text file
         with open('morse_to_text.txt', 'a+') as f:
             f.write(decipher + '\n')
     except PermissionError:
         print('Not adequate access rights.')
 
-
+# morse code dictionary used for translation from text to morse
 morse_code_dict_cz = {
     'A': '.-', 'Á': '.-', 'B': '-...', 'C': '-.-.', 'Č': '-.-.', 'D': '-..', 'Ď': '-..', 'E': '.', 'É': '.', 'Ě': '.',
     'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'Í': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--',
@@ -66,6 +77,7 @@ morse_code_dict_cz = {
     '&': '.-...', "'": '.----.', '@': '.--.-.', ')': '-.--.-', '(': '-.--.', ':': '---...', ',': '--..--', '=': '-...-',
     '!': '-.-.--', '.': '.-.-.-', '-': '-....-', '+': '.-.-.',  '"': '.-..-.', '?': '..--..', '/': '-..-.'}
 
+# morse code dictionary used for translation from morse to text
 morse_code_dict_int = {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..',
     'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
