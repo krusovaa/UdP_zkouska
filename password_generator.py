f6517b515f6a357cb0e3430827aeed7cfd6b762e
Anna Krusova <anicka.krusova@gmail.com>
2020-02-18 20:15:35 +0100

    final version

--- password_generator.py
+++ password_generator.py
@@ -4,24 +4,31 @@ import string
 
 
 def get_char():
-    """creates list of characters from which the combinations of password are going to be made of"""
+    """create list of characters from which the combinations of password are going to be made of"""
+    # list of lowercase characters
     lowers = list(string.ascii_lowercase)
+    # list of uppercase characters
     uppers = list(string.ascii_uppercase)
+    # list of numbers
     numbers = [str(i) for i in range(1, 10)]
     all_char = lowers + uppers + numbers
+    print(len(all_char))
     return all_char
 
 
 def input_length():
-    """checks the right input of the length of the password"""
+    """check the right input of the length of the password"""
     print('How long do you want your password combinations to be?')
     while True:
         inp = input('Enter an integer between 2 and 6: ')
         try:
+            # check the right type of input (integer)
             inp = int(inp)
+        # exception treatment
         except ValueError:
             print('Invalid integer.')
             continue
+        # check the input in the right range (2 - 6)
         if 2 <= inp <= 6:
             break
         else:
@@ -30,12 +37,15 @@ def input_length():
 
 
 def pw_gen(characters, length):
-    """generates all characters combinations with selected length and exports them to a text file"""
+    """generate all characters combinations with selected length and export them to a text file"""
     try:
+        # output text file
         with open("password_combinations.txt", "a+") as f:
-            for i in itertools.product(characters, repeat=length):
-                combination = ''.join(i)
+            for p in itertools.product(characters, repeat=length):
+                combination = ''.join(p)
+                # write each combination and create a new line
                 f.write(combination + '\n')
+    # exception treatment
     except PermissionError:
         print('Not adequate access rights: ', f)
 
